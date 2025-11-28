8b7338fa9b7aa503461ab1ccfcbb239ab42dd951
wajibro <wajibro@yahoo.com>
2025-11-28 09:54:31 +0700

    Membenarkan sistem oop loadscreen

--- pages/loadscreen.py
+++ pages/loadscreen.py
@@ -1,7 +1,14 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
-from src import fungsi
+def import_image(src):
+  x = Image.open(src)
+  return ImageTk.PhotoImage(x)
+
+def import_imageRGBA(src):
+  x = Image.open(src).convert('RGBA')
+  return ImageTk.PhotoImage(x)
+
 
 def splash1(self):
   self.bg= fungsi.import_image('assets/background-start.png')
--- pages/src/fungsi.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageTk
-
-def import_image(src):
-  x = Image.open(src)
-  return ImageTk.PhotoImage(x)
-
-def import_imageRGBA(src):
-  x = Image.open(src).convert('RGBA')
-  return ImageTk.PhotoImage(x)
