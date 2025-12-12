2e8997f795863b589a7f0d91834c8f5f11fd1142
upi <wajibro@yahoo.com>
2025-12-12 09:15:35 +0700

    Merapikan kode

--- src/buy_props.py
+++ src/buy_props.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-import numpy as np
 from PIL import Image, ImageTk
 
 def import_image(src, resize= None, png= None): # Fungsi untuk mengimport gambar
